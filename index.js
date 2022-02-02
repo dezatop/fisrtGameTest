39916b2643f443cb9f3f6519489d73e5658c9911
dezatop <toxashark@gmail.com>
2022-02-02 16:20:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const canvas = document.getElementById('main__canvas')
+    const ctx = canvas.getContext('2d')
+    const CANVAS_WIDTH = canvas.width = 600
+    const CANVAS_HEIGHT = canvas.height = 600
+
+    const playerImg = new Image()
+});
